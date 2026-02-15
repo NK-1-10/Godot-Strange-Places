11f0efdbccff7e79fdc8bc28d12583fdf06528be
NK-1-10 <kanepe.nikola@gmail.com>
2026-02-15 19:30:31 +0200

    character can now double jump if keypad 7 is pressed

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,15 +4,24 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var jumps = 0;
+var jump_max = 2;
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and $"..".doubleJump == false:
+		velocity.y = JUMP_VELOCITY
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jumps < jump_max and $"..".doubleJump == true:
+		jumps = jumps+1;
 		velocity.y = JUMP_VELOCITY
+		if is_on_floor():
+			jumps = 0;
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- main.gd
+++ main.gd
@@ -2,12 +2,15 @@ extends Node2D
 var doubleJump = false;
 var dash = false;
 
+func _doubleUp():
+	pass
 
 func _input(event):
 	if event.is_action_pressed("Double_jump"):
 		if doubleJump == false:
 			$Basic.modulate = Color(0.148, 0.481, 0.85, 1.0)
 			doubleJump = true
+			dash = false
 		else:
 			$Basic.modulate = Color(1.0, 1.0, 1.0, 1.0)
 			doubleJump = false
@@ -15,6 +18,7 @@ func _input(event):
 	if event.is_action_pressed("Dash"):
 		if dash == false:
 			$Basic.modulate = Color(0.847, 0.239, 0.133, 1.0)
+			doubleJump = false
 			dash = true
 		else:
 			$Basic.modulate = Color(1.0, 1.0, 1.0, 1.0)
