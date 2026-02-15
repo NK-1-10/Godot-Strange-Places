bc4f4c206c02745c786faa929f5ebf9a90dd4e2a
NK-1-10 <kanepe.nikola@gmail.com>
2026-02-15 17:19:48 +0200

    rect changes shape. is unfortunately a bit buggy

---
+++ main.gd
@@ -0,0 +1,21 @@
+extends Node2D
+var doubleJump = false;
+var dash = false;
+
+
+func _input(event):
+	if event.is_action_pressed("Double_jump"):
+		if doubleJump == false:
+			$Basic.modulate = Color(0.148, 0.481, 0.85, 1.0)
+			doubleJump = true
+		else:
+			$Basic.modulate = Color(1.0, 1.0, 1.0, 1.0)
+			doubleJump = false
+			
+	if event.is_action_pressed("Dash"):
+		if dash == false:
+			$Basic.modulate = Color(0.847, 0.239, 0.133, 1.0)
+			dash = true
+		else:
+			$Basic.modulate = Color(1.0, 1.0, 1.0, 1.0)
+			dash = false
