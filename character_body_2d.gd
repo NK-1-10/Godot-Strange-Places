4bf47655ef9e508c7ca80cef3bc16ad52f1f54bd
NK-1-10 <kanepe.nikola@gmail.com>
2026-02-18 16:49:01 +0200

    Anti-G funkcija un var pārvērsties zaļš!

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -10,6 +10,7 @@ const Deceleration = 0.2
 var Wants_jump = false
 var Can_Jump = false
 
+var AntiG = false;
 var doubleJump = true;
 var dash = false;
 
@@ -20,11 +21,33 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	velocity += get_gravity() * delta
 	
+	if AntiG == false:
+		velocity += get_gravity() * delta
+		up_direction = Vector2.UP
+	
+	if AntiG == true:
+		velocity += get_gravity() * delta * -1
+		up_direction = Vector2.DOWN
+	
+	
+	if Input.is_action_just_pressed("ReverseGravity"):
+		if AntiG == true:
+			AntiG = false;
+			SetColor()
+		elif AntiG == false:
+			AntiG = true;
+			SetColor()
+
+
 	#Ejam augšā un palaiž vaļā
-	if Input.is_action_just_released("Jump") and velocity.y < 0:
-		velocity.y *= 0.5 
+	if Input.is_action_just_released("Jump"):
+		if AntiG == false and velocity.y <0:
+			velocity.y *= 0.5
+		elif AntiG == true and velocity.y > 0:
+			velocity.y *= 0.5
+	
+		
 	
 	
 	if(is_on_floor()):
@@ -45,12 +68,15 @@ func _physics_process(delta: float) -> void:
 		get_node("JumpBuffer").start()
 	
 	if Wants_jump:
+		var jump_force = JUMP_VELOCITY
+		if AntiG:
+			jump_force = -JUMP_VELOCITY #I think šitas dabū upside down jump. correct me if im wrong :>
 		if Can_Jump:
 			Wants_jump = false
 			Can_Jump = false
-			velocity.y = JUMP_VELOCITY
+			velocity.y = jump_force
 		elif !PowerUsed && doubleJump:
-			velocity.y = JUMP_VELOCITY
+			velocity.y = jump_force
 			Wants_jump = false
 			PowerUsed = true
 			SetColor()
@@ -80,6 +106,8 @@ func _on_jump_grace_timeout() -> void:
 func SetColor():
 	if(PowerUsed):
 		$ColorRect.color = Color(1.0, 1.0, 1.0, 1.0)
+	elif AntiG:
+		$ColorRect.color = Color(0.314, 0.745, 0.318, 1.0)
 	elif doubleJump:
 		$ColorRect.color = Color(0.847, 0.239, 0.133, 1.0)
 	else:
